42f64b04f618965e5c139b5177bf73899405a85a
Sam Gryzick <samuelgriz@yahoo.com>
2022-05-05 15:00:22 -0600

    serialize user fix

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ passport.use(new GoogleStrategy({
 }))
 
 passport.serializeUser(function(user, done) {
-    done(null, user.id);
+    done(null, user);
 });
   
 passport.deserializeUser(function(id, done) {
